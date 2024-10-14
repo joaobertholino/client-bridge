0bf92576fbf39c3f2aea466954bb0a93d0c67b6c
João Bertholino <joaobertholino964@gmail.com>
2024-10-13 21:23:35 -0300

    fix: Adding the Transaction Annotation

--- src/main/java/dev/joaobertholino/clientbridge/service/TransactionService.java
+++ src/main/java/dev/joaobertholino/clientbridge/service/TransactionService.java
@@ -2,7 +2,9 @@ package dev.joaobertholino.clientbridge.service;
 
 import dev.joaobertholino.clientbridge.request.TransactionRequest;
 import dev.joaobertholino.clientbridge.response.TransactionResponse;
+import jakarta.transaction.Transactional;
 
 public interface TransactionService {
+	@Transactional
 	TransactionResponse makeTransaction(TransactionRequest transactionRequest);
 }
