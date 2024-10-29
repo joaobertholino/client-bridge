6f9fa8eda93decf188e8e4c6c589b70edccc6d09
João Bertholino <joaobertholino964@gmail.com>
2024-10-29 13:23:15 -0300

    feat: implements NGINX load balancer and gateway.

---
+++ src/main/java/dev/joaobertholino/clientbridge/controller/RootController.java
@@ -0,0 +1,27 @@
+package dev.joaobertholino.clientbridge.controller;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class RootController {
+
+	@Operation(
+			summary = "Inserts a new customer.",
+			description = "Inserts a new customer according to the contents of the requisition body, returning the inserted customer.")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "200", description = "System root resource", content = @Content(schema = @Schema(implementation = String.class), mediaType = "text/plain"))})
+	@GetMapping(produces = "application/json")
+	public ResponseEntity<String> root() {
+		return ResponseEntity.status(HttpStatus.OK).body("This is the root of the system :)");
+	}
+}
